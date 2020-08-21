725a2c21b966633e4e6362b547494dbe91211739
youpvl <youpvleeuwen@gmail.com>
2020-08-21 12:20:47 +0200

    Added order for the blogc

--- src/Repository/BlogRepository.php
+++ src/Repository/BlogRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Blog|null find($id, $lockMode = null, $lockVersion = null)
  * @method Blog|null findOneBy(array $criteria, array $orderBy = null)
- * @method Blog[]    findAll()
  * @method Blog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class BlogRepository extends ServiceEntityRepository
@@ -17,6 +16,14 @@ class BlogRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Blog::class);
+    }
+
+    public function findAll ()
+    {
+        return $this->createQueryBuilder('b')
+            ->orderBy('b.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
     }
 
     // /**
